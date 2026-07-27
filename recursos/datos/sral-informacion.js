a265115bd33c02de6bb4b4b852b277461ef23d95
Sebastián Alcántara <67203068+sebastianr-alcantara@users.noreply.github.com>
2026-07-26 23:23:25 -0500

    datos-experiencia
    Datos de experiencia en formato json

---
+++ recursos/datos/sral-informacion.js
@@ -0,0 +1,88 @@
+var informacion = {
+    "experience": [
+        {
+            "id": 1,
+            "departamento": "Piura",
+            "provincia": ["Paita"],
+            "sector": "Drenaje Pluvial",
+            "empresa": "CISPDR",
+            "modalidad":"Presencial",
+            "proyecto": "Drenaje Pluvial de la Ciudad de Paita",
+            "cargo": "Analista GIS",
+            "nivel": "Analista",
+            "inicio": "2021-09-22",
+            "final": "2022-04-07",
+            "habilidades": ["Proactivo"]
+        },
+        {
+            "id": 2,
+            "departamento": "Lima",
+            "provincia": ["Lima"],
+            "sector": "Telecomunicaciones",
+            "empresa": "COBRA PERU",
+            "modalidad":"Remoto",
+            "proyecto": "Proyectos a Nivel Corporativo",
+            "cargo": "Coordinador GIS",
+            "nivel": "Coordinador",
+            "inicio": "2022-07-08",
+            "final": "2023-09-30",
+            "habilidades": ["Proactivo","Eficiencia"]
+        },
+        {
+            "id": 3,
+            "departamento": "Cusco",
+            "provincia": ["Cusco"],
+            "sector": "Drenaje Pluvial",
+            "empresa": "SEGCONSULT",
+            "modalidad":"Presencial",
+            "proyecto": "Catastro de Drenaje Pluvial de la Ciudad de Cuzco",
+            "cargo": "Coordinador Técnico GIS/CAD",
+            "nivel": "Coordinador",
+            "inicio": "2023-02-14",
+            "final": "2023-09-30",
+            "habilidades": ["Proactivo","Liderazgo"]
+        },
+        {
+            "id": 4,
+            "departamento": "Cusco",
+            "provincia": ["Cusco"],
+            "sector": "Drenaje Pluvial",
+            "empresa": "CIC",
+            "modalidad":"Presencial",
+            "proyecto": "Catastro de Drenaje Pluvial de la Ciudad de Cuzco",
+            "cargo": "Coordinador GIS/CAD",
+            "nivel": "Coordinador",
+            "inicio": "2023-10-02",
+            "final": "2024-01-20",
+            "habilidades": ["Proactivo","Liderazgo"]
+        },
+        {
+            "id": 5,
+            "departamento": "Lambayeque",
+            "provincia": ["Chiclayo","Lambayeque"],
+            "sector": "Saneamiento de Agua y Desagüe",
+            "empresa": "EPSEL",
+            "modalidad":"Presencial",
+            "proyecto": "Catastro Comercial de EPSEL en el Departamento de Lambayeque",
+            "cargo": "Servicios Específicos",
+            "nivel": "Coordinador",
+            "inicio": "2024-07-05",
+            "final": "2024-12-31",
+            "habilidades": ["Proactivo","Liderazgo"]
+        },
+        {
+            "id": 6,
+            "departamento": "Lima",
+            "provincia": ["Lima"],
+            "sector": "Inmobiliario",
+            "empresa": "LOS PORTALES",
+            "modalidad":"Presencial",
+            "proyecto": "Proyectos a Nivel Corporativo",
+            "cargo": "Analista GIS",
+            "nivel": "Analista",
+            "inicio": "2025-01-02",
+            "final": "2026-07-26",
+            "habilidades": ["Proactivo", "Innovación"]
+        }
+    ]
+}
