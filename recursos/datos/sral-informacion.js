c2879b1f58bc2ea8d9adc59124bec55abee60578
Sebastián Alcántara <67203068+sebastianr-alcantara@users.noreply.github.com>
2026-07-28 19:15:12 -0500

    datos-actualizados
    NUEVA ESTRUCTURA

--- recursos/datos/sral-informacion.js
+++ recursos/datos/sral-informacion.js
@@ -1,88 +1,154 @@
-var informacion = {
-    "experience": [
+var information = {
+    summary: [
+        `<p>Soy <strong>Ingeniero Geógrafo titulado</strong> con mención en Geomática y Ordenamiento Territorial 
+        por la Universidad Nacional Mayor de San Marcos, con más de <strong>4 años de experiencia</strong> en el uso de 
+        Sistemas de Información Geográfica aplicados a Catastro, Drenaje Pluvial y, actualmente, al desarrollo inmobiliario.
+        <br>
+        Cuento con experiencia en análisis espacial, desarrollo de soluciones GIS y procesamiento de información territorial 
+        mediante herramientas de programación y técnicas de recopilación de datos. Asimismo, realizo planificación y ejecución
+        de vuelos fotogramétricos, contando con <strong>licencia para la operación de RPAS/Drones</strong>.</p>`
+    ],
+    education: [
         {
-            "id": 1,
-            "departamento": "Piura",
-            "provincia": ["Paita"],
-            "sector": "Drenaje Pluvial",
-            "empresa": "CISPDR",
-            "modalidad":"Presencial",
-            "proyecto": "Drenaje Pluvial de la Ciudad de Paita",
-            "cargo": "Analista GIS",
-            "nivel": "Analista",
-            "inicio": "2021-09-22",
-            "final": "2022-04-07",
-            "habilidades": ["Proactivo"]
+            school_education: [
+                {
+                    id: 1,
+                    state: 'Finalizado',
+                    level: 'Primaria',
+                    institution: 'Manuel Scorza Tello',
+                    start: '2003-03-01',
+                    end: '2003-12-15'
+                },
+                {
+                    id: 2,
+                    state: 'Finalizado',
+                    level: 'Primaria',
+                    institution: 'Miguel Grau Seminario 6032',
+                    start: '2004-03-01',
+                    end: '2008-12-15'
+                },
+                {
+                    id: 3,
+                    state: 'Finalizado',
+                    level: 'Secundaria',
+                    institution: 'Peru Birf - República del Ecuador',
+                    start: '2009-03-01',
+                    end: '2013-12-15'
+                }
+            ],
+            higher_education: [
+                {
+                    undergraduated: {
+                        id: 1,
+                        state: 'Finalizado',
+                        level: 'Superior',
+                        university: 'Universidad Nacional Mayor de San Marcos',
+                        career: 'Ingeniería Geográfica',
+                        start: '2017-03-27',
+                        end: '2022-02-09',
+                        bachelor: '2022-06-22',
+                        title: '2025-11-28',
+                        thesys: 'Determinación de Ecosistemas Costeras mediante Agrupamiento de Bandas Espectrales'
+                    },
+                    postgraduated: {
+                        id: 1,
+                        state: 'En planificación',
+                        level: 'Maestría',
+                        university: 'Edinburgh University',
+                        specialization: 'Computer Sciencie and Earth Observation',
+                        start: null,
+                        end: null,
+                        title: null,
+                        thesys: null
+                    }
+                }
+            ]
+        }
+    ],
+    experience: [
+        {
+            id: 1,
+            department: "Piura",
+            province: ["Paita"],
+            sect: "Drenaje Pluvial",
+            company: "CISPDR",
+            mode:"Presencial",
+            summary: "Drenaje Pluvial de la Ciudad de Paita",
+            job: "Analista GIS",
+            level: "Analista",
+            start: "2021-09-22",
+            end: "2022-04-07",
+            skills: ["Proactivo"]
         },
         {
-            "id": 2,
-            "departamento": "Lima",
-            "provincia": ["Lima"],
-            "sector": "Telecomunicaciones",
-            "empresa": "COBRA PERU",
-            "modalidad":"Remoto",
-            "proyecto": "Proyectos a Nivel Corporativo",
-            "cargo": "Coordinador GIS",
-            "nivel": "Coordinador",
-            "inicio": "2022-07-08",
-            "final": "2023-09-30",
-            "habilidades": ["Proactivo","Eficiencia"]
+            id: 2,
+            department: "Lima",
+            province: ["Lima"],
+            sect: "Telecomunicaciones",
+            company: "COBRA PERU",
+            mode:"Remoto",
+            summary: "Proyectos a Nivel Corporativo",
+            job: "Coordinador GIS",
+            level: "Coordinador",
+            start: "2022-07-08",
+            end: "2023-09-30",
+            skills: ["Proactivo","Eficiencia"]
         },
         {
-            "id": 3,
-            "departamento": "Cusco",
-            "provincia": ["Cusco"],
-            "sector": "Drenaje Pluvial",
-            "empresa": "SEGCONSULT",
-            "modalidad":"Presencial",
-            "proyecto": "Catastro de Drenaje Pluvial de la Ciudad de Cuzco",
-            "cargo": "Coordinador Técnico GIS/CAD",
-            "nivel": "Coordinador",
-            "inicio": "2023-02-14",
-            "final": "2023-09-30",
-            "habilidades": ["Proactivo","Liderazgo"]
+            id: 3,
+            department: "Cusco",
+            province: ["Cusco"],
+            sect: "Drenaje Pluvial",
+            company: "SEGCONSULT",
+            mode:"Presencial",
+            summary: "Catastro de Drenaje Pluvial de la Ciudad de Cuzco",
+            job: "Coordinador Técnico GIS/CAD",
+            level: "Coordinador",
+            start: "2023-02-14",
+            end: "2023-09-30",
+            skills: ["Proactivo","Liderazgo"]
         },
         {
-            "id": 4,
-            "departamento": "Cusco",
-            "provincia": ["Cusco"],
-            "sector": "Drenaje Pluvial",
-            "empresa": "CIC",
-            "modalidad":"Presencial",
-            "proyecto": "Catastro de Drenaje Pluvial de la Ciudad de Cuzco",
-            "cargo": "Coordinador GIS/CAD",
-            "nivel": "Coordinador",
-            "inicio": "2023-10-02",
-            "final": "2024-01-20",
-            "habilidades": ["Proactivo","Liderazgo"]
+            id: 4,
+            department: "Cusco",
+            province: ["Cusco"],
+            sect: "Drenaje Pluvial",
+            company: "CIC",
+            mode:"Presencial",
+            summary: "Catastro de Drenaje Pluvial de la Ciudad de Cuzco",
+            job: "Coordinador GIS/CAD",
+            level: "Coordinador",
+            start: "2023-10-02",
+            end: "2024-01-20",
+            skills: ["Proactivo","Liderazgo"]
         },
         {
-            "id": 5,
-            "departamento": "Lambayeque",
-            "provincia": ["Chiclayo","Lambayeque"],
-            "sector": "Saneamiento de Agua y Desagüe",
-            "empresa": "EPSEL",
-            "modalidad":"Presencial",
-            "proyecto": "Catastro Comercial de EPSEL en el Departamento de Lambayeque",
-            "cargo": "Servicios Específicos",
-            "nivel": "Coordinador",
-            "inicio": "2024-07-05",
-            "final": "2024-12-31",
-            "habilidades": ["Proactivo","Liderazgo"]
+            id: 5,
+            department: "Lambayeque",
+            province: ["Chiclayo","Lambayeque"],
+            sect: "Saneamiento de Agua y Desagüe",
+            company: "EPSEL",
+            mode:"Presencial",
+            summary: "Catastro Comercial de EPSEL en el Departamento de Lambayeque",
+            job: "Servicios Específicos",
+            level: "Coordinador",
+            start: "2024-07-05",
+            end: "2024-12-31",
+            skills: ["Proactivo","Liderazgo"]
         },
         {
-            "id": 6,
-            "departamento": "Lima",
-            "provincia": ["Lima"],
-            "sector": "Inmobiliario",
-            "empresa": "LOS PORTALES",
-            "modalidad":"Presencial",
-            "proyecto": "Proyectos a Nivel Corporativo",
-            "cargo": "Analista GIS",
-            "nivel": "Analista",
-            "inicio": "2025-01-02",
-            "final": "2026-07-26",
-            "habilidades": ["Proactivo", "Innovación"]
+            id: 6,
+            department: "Lima",
+            province: ["Lima"],
+            sect: "Inmobiliario",
+            company: "LOS PORTALES",
+            mode:"Presencial",
+            summary: "Proyectos a Nivel Corporativo",
+            job: "Analista GIS",
+            level: "Analista",
+            start: "2025-01-02",
+            end: "2026-07-26",
+            skills: ["Proactivo", "Innovación"]
         }
     ]
 }
